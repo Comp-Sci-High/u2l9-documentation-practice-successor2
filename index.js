99572256d27db5b790751787081834ce857e3d9e
successor2 <110046839+successor2@users.noreply.github.com>
2024-10-25 17:03:36 +0000

    Task 1&2 Done

--- index.js
+++ index.js
@@ -3,13 +3,17 @@
 // The two APIs you will work with are:
 // 1. The "People in Space" API, which gives information about the people currently in outer space.
 // 2. The NASA Astronomy Picture of the Day (APOD) API, which provides a daily picture or video related to astronomy.
-
+let requestURL = "http://api.open-notify.org/astros.json"
 async function fetchAstronauts(requestURL) {
-        
+    const response = await fetch(requestURL);
+    const data = await response.json();
+    console.log(data.people[7].name)
+    return data;
+
 }
 
 // CALL your function here
-
+fetchAstronauts(requestURL)
 // TASK 1: Fetch Data from the "People in Space" API
 
 // 1. Find the "People in Space" API: http://open-notify.org/
@@ -23,14 +27,20 @@ async function fetchAstronauts(requestURL) {
 // TASK 2: Fetch Data from the NASA Astronomy Picture of the Day (APOD) API
 
 // 1. Find the "Astronomy Picture of the Day" API: https://api.nasa.gov/
-// 2. Identify whether you need to be authenticated with an API Key, if so create an account and save your key. 
+// 2. Identify whether you need to be authenticated with an API Key, if so create an account and save your key.
 // 3. Navigate the documenation and create a request URL to fetch a list of all the people in outer space.
-// 4. Make a fetch request and format your resulting JSON. 
+// 4. Make a fetch request and format your resulting JSON.
 // 5. Log the data to the console to inspect its structure.
 // 6. Modify the console log to only show the URL of today's astronomy picture. 
+let reqURL = "https://api.nasa.gov/planetary/apod?api_key=8ipaCLkbUh44TQ8iG1JpEfrAwsxtOgIDUjRdXuZI"
 
-async function fetchPictures(requestURL) {
-        
+
+async function fetchPictures(reqURL) {
+    const response = await fetch(reqURL);
+    const data = await response.json();
+    console.log(data.url)
+    return data;
 }
 
 // CALL your function here
+fetchPictures(reqURL)
